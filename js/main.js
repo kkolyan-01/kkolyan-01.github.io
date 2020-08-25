e90d4f6403a1883e6c643d8d6561a038cb7ec5f7
kkolyan-01 <64558680+kkolyan-01@users.noreply.github.com>
2020-08-25 18:28:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ $(function () {
 		$('.header__menu-list').slideToggle();
 	})
 
+	$('.header__menu-item').on('click', function () {
+		if (window.innerWidth <= 1100) {
+			$('.header__menu-list').slideToggle();
+			$('.checkbox1').prop("checked", false);
+		}
+
+	})
 });
